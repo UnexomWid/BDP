847d1a7196570fcd8b1d7dec924e4c2113d0326b
UnexomWid <mihaitinu@gmail.com>
2019-08-26 13:31:42 +0300

    Added license.

--- src/bdp.cpp
+++ src/bdp.cpp
@@ -1,3 +1,23 @@
+/*
+ * BDP - A basic 64 bit packaging format for binary data.
+ * Copyright (C) 2019  UnexomWid
+
+ * bdp.h - Contains the declaration of the library.
+
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #include "bdp.h"
 
 bdp::Header::Header(uint8_t nlbs, uint8_t vlbs) : NAME_MAX_LENGTH(getMaxLength(nlbs)),
